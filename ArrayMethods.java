35e1a350759b21f2e087d9cdf6b935b9c62605b8
Derek Lao <dlao00@stuy.edu>
2018-11-05 20:39:59 -0500

    added first four methods

---
+++ ArrayMethods.java
@@ -0,0 +1,75 @@
+public class ArrayMethods{
+
+   /*
+   *PART 1
+   */
+   public static int rowSum(int[][] ary, int x){
+     int sum=0;
+     for(int i=0;i<ary[x].length;i++)
+     {
+       sum+=ary[x][i];
+     }
+     return sum;
+   }
+    //returns the sum of the elements in Row x of ary.
+
+   public static int columnSum(int[][] ary, int x){
+     int sum=0;
+     for(int i=0;i<ary.length;i++)
+     {
+       if(x>=ary[i].length)
+       {
+         continue;
+       }
+       sum+=ary[i][x];
+     }
+     return sum;
+   }
+    //returns the sum of the elements in Column x of ary (careful with rows of different lengths!).
+
+   /*
+   *PART 2 - use prior methods where appropriate
+   */
+   public static int[] allRowSums(int[][] ary){
+     int[] answer=new int[ary.length];
+     for(int i=0;i<ary.length;i++)
+     {
+       answer[i]=rowSum(ary,i);
+     }
+     return answer;
+   }
+    //returns an array of the row sums for each row of ary.
+    //Index i of the return array contains the sum of elements in row i.
+
+    public static int[] allColSums(int[][] ary){
+      int columnlength=0;
+      for(int i=0;i<ary.length;i++)
+      {
+        if(ary[i].length>columnlength)
+        {
+          columnlength=ary[i].length;
+        }
+      }
+      int[] answer=new int[columnlength];
+      for(int i=0;i<columnlength;i++)
+      {
+        answer[i]=columnSum(ary,i);
+      }
+      return answer;
+    }
+    //Returns an array with the column sum of each column of ary.
+    //When a row is not long enough to reach the column count it as a zero. (NO indexOutOfBounds should ever occur)
+    //Index i of the return array contains the sum of elements in column i, ignoring any rows that are too short.
+    //The length of the returned array should be the length of the LONGEST array in ary.
+
+
+   /*
+   *PART 3 - use prior methods where appropriate
+   */
+   public static boolean isRowMagic(int[][] ary){}
+     //checks if the array is row-magic (this means that every row has the same row sum).
+
+   public static boolean isColumnMagic(int[][] ary){}
+    //checks if the array is column-magic (this means that every column has the same column sum).
+
+}
